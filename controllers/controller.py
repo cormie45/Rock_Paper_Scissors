06e765e68c59d0e8d1d2fe07900cb8bf4b607aac
Steven Cormack <stevec@Stevens-MBP.lan>
2021-04-16 23:42:52 +0100

    more 404 issues

--- controllers/controller.py
+++ controllers/controller.py
@@ -7,3 +7,7 @@ from modules.player import Player
 def index():
     return render_template('index.html', title='Rock Paper Scissors')
 
+@app.route('/tprps')
+def tprps():
+    return render_template('/tprps.html', title='2 Player Rock Paper Scissors')
+
