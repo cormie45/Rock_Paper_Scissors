9c7b6620af7ba210885a3d402767138a6fb96454
Steven Cormack <stevec@Stevens-MBP.lan>
2021-04-18 00:33:02 +0100

    trying to make computer player work

--- controllers/controller.py
+++ controllers/controller.py
@@ -33,9 +33,34 @@ def tprps_form():
 def tprpsls():
     return render_template('/tprpsls.html', title='2 Player Rock Paper Scissors Lizard Spock')
 
+@app.route('/tprpsls', methods = ["POST"])
+def tprpsls_form():
+    player1_name = request.form['name1']
+    choice_1 = request.form['player1_choice']
+    player2_name = request.form['name2']
+    choice_2 = request.form['player2_choice']
+    player_1 = Player(name=player1_name)
+    player_1.make_choice(choice_1)
+    player_2 = Player(name=player2_name)
+    player_2.make_choice(choice_2)
+    play_game = Game(player_1, player_2)
+    result = play_game.result(player_1, player_2)
+    return render_template('score2.html', title='Winner Is', result=result, player_1=player_1, player_2=player_2)
+
 @app.route('/oprps')
 def oprps():
     return render_template('/oprps.html', title='1 Player Rock Paper Scissors')
+
+@app.route('/oprps', methods = ["POST"])
+def oprps_form():
+    player1_name = request.form['name1']
+    choice_1 = request.form['player1_choice']
+    player_1 = Player(name=player1_name)
+    player_1.make_choice(choice_1)
+    player_2 = Game.computer_rps()
+    play_game = Game(player_1, player_2)
+    result = play_game.result(player_1, player_2)
+    return render_template('score.html', title='Winner Is', result=result, player_1=player_1, player_2=player_2)
 
 @app.route('/oprpsls')
 def oprpsls():
